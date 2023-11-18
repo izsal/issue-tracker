07a17cc32cec7a7d071d621e5516fecd3ba7b144
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2023-11-18 15:18:54 +0700

    Set Up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
   return (
     <div>
-      IssuesPage
-      <div></div>
+      <Button color="cyan" variant="soft">
+        New Issues
+      </Button>
     </div>
   );
 };
